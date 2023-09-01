60e8129c74eceecfca4397bdcb939c91e7f70371
VMH2k3 <vmh300603@gmail.com>
2023-09-01 13:30:40 +0700

    continue learn

--- demo.php
+++ demo.php
@@ -67,15 +67,50 @@ $bien = '1,2,3';
 // }
 
 // die(Dừng chương trình);exit(); không thực hiện nhưng câu lệnh dưới chúng
-  
-echo "Hello guy";
 //câu lệnh include và repquire
 // include('đường dẫn đến file cần liên kết'); 
 // include "index.php"; thêm file index vào nếu lỗi thì các câu lệnh dưới cẫn chạy
 // require "index.php"; nếu lỗi thì các câu lệnh dưới sẽ dừng lại
 
-//gán chuỗi cho hằng
-define('_AGENCY','VŨ Minh Hoãng');
+
+
+//XỬ LÍ CHUỖIIIIIII 
+
+
+// //gán chuỗi cho hằng
+// define('_AGENCY','VŨ Minh Hoãng');
+// echo "Hello mình là \"Hoàng\""
+
+//hàm lặp chuỗi n lần
+// echo str_repeat('Hoàng FA !  ',4);
+
+//hàm tìm chuỗi và thay thế chuỗi
+// echo str_replace('-','<3','Hoàng đẹp trai vã ọ -');
+ 
+//hàm mã hoá MD5 chuỗi $str(32 kí tự)
+// echo md5('123456');
+
+//hàm mã hoá sha1 chuỗi $str(40 kí tự)
+// echo sha1('123456');
+
+//hàm cắt chuỗi thành chuỗi con
+// $bien = "Hoàng vô cùng đẹp trai!";
+// $biencon = substr($bien, 0, 6);
+// echo $biencon;
+
+//hàm tìm và cắt từ kí tự tìm được từ trong hàm cho đến hết
+// $bien = "Hoàng vô cùng đẹp trai!";
+// $bien_moi = strstr($bien, 'Hoàng');
+// echo $bien_moi;
+
+//hàm tìm vị trị của substr nằm trong str đưa ra vị trí nếu có ngược lại đưa ra false
+// $bien = "Hoàng vô cùng đẹp trai!";
+// echo strpos($bien, 'vô cùng');
+
+//hàm cắt bỏ substr cũ trong str thay bằng substr mới (gần giống replace nhưng là chuỗi con)
+// $bien = "Hoàng vô cùng đẹp trai!";
+// echo substr_replace($bien, 'vã ọ',7,9);
+// chú ý cú pháp ($str, chuỗi thay thế, vị trí bắt đầu thay thế, độ dài chuỗi bị thay thế);
 
 
 ?>
